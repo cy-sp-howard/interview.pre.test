f5fae2e630548185ee32da5e83fbb55927bfef32
cy-sp-howard <cy.sp.howard@gmail.com>
2024-06-26 21:52:48 +0800

    sub route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory('/pre-test'),
+  history: createWebHashHistory('/pre-test/'),
   routes,
 })
 export default router
